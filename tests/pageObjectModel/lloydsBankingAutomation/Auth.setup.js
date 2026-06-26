d51ae07f8acaf9f3b196ef15cb1ead319fc76124
Archana <kumariarchana70@gmail.com>
2026-06-26 17:17:00 +0100

    modified auth file

--- tests/pageObjectModel/lloydsBankingAutomation/Auth.setup.js
+++ tests/pageObjectModel/lloydsBankingAutomation/Auth.setup.js
@@ -2,7 +2,7 @@ import {test} from '@playwright/test';
 test('verify the authentication page', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
     await page.getByPlaceholder('Username').fill('Admin');
-    await page.getByPlaceholder('Password').fill('admin12');
+    await page.getByPlaceholder('Password').fill('admin123');
     await page.getByRole('button', { name: 'Login' }).click();
     await page.waitForURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
     await page.getByRole('heading', { name: 'Dashboard' }).isVisible();
