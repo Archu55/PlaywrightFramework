cde19bfc827ea963b5151becb17759d3eac5ace5
Archana <kumariarchana70@gmail.com>
2026-08-18 20:35:33 +0100

    modified jenkinsFileDocker

--- jenkinsFIleDocker.groovy
+++ jenkinsFIleDocker.groovy
@@ -104,13 +104,13 @@ pipeline {
 
         stage('Build Docker Image') {
             steps {
-                sh 'docker build -t playwright-tests .'
+            bat 'docker build -t playwright-tests .'
             }
         }
 
         stage('Run Playwright Tests') {
             steps {
-                sh 'docker run --rm -e HEADLESS="%HEADLESS%" playwright-tests'
+                bat 'docker run --rm -e HEADLESS="%HEADLESS%" playwright-tests'
             }
         }
         stage('Generate & Archive Reports') {
